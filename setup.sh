5b621e808774b1cc5a6c4758e541f00d004bcc90
Josh Dietrich <joshuadietrich256@gmail.com>
2025-09-04 13:58:43 -0400

    adding tpm to setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 source_file="$(dirname $BASH_SOURCE)/.tmux.conf"
 echo "Moving $source_file to root"
 cp $source_file $HOME/
+
+git clone git@github.com:tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
+
+
+
